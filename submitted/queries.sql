f8edadb26055fce67ff3d7f2be7ffe5f3a2eff93
beezmo <37284613+beezmo@users.noreply.github.com>
2019-07-22 15:10:22 -0700

    Added query to return dept manager info

--- submitted/queries.sql
+++ submitted/queries.sql
@@ -7,8 +7,8 @@ SELECT
 	e.gender, 
 	s.salary
 FROM salaries s
-JOIN employees e ON
-e.emp_no = s.emp_no;
+JOIN employees e 
+ON e.emp_no = s.emp_no;
 
 -- List employees who were hired in 1986.
 SELECT *
@@ -18,3 +18,14 @@ WHERE (
 	hire_date <= '1986/12/31'
 );
 
+-- List the manager of each department with the following info: 
+-- dept number, dept name, the manager's employee number, 
+-- last name, first name, and start and end employment dates.
+-- NB: HIRE DATE BELOW DOES NOT MEAN START DATE OF MGMT POSITION
+SELECT m.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name,
+	e.hire_date, m.to_date
+FROM dept_manager m
+LEFT JOIN departments d
+ON m.dept_no = d.dept_no
+LEFT JOIN employees e
+ON e.emp_no = m.emp_no;
