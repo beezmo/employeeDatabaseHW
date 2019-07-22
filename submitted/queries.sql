ede19df392df10251c5630c9380986128af9c583
beezmo <37284613+beezmo@users.noreply.github.com>
2019-07-22 14:48:38 -0700

    Added query to show employees hired in '86

--- submitted/queries.sql
+++ submitted/queries.sql
@@ -1,11 +1,20 @@
 -- List the following details of each employee: 
 -- employee number, last name, first name, gender, and salary.
-select 
+SELECT 
 	e.emp_no, 
 	e.last_name, 
 	e.first_name, 
 	e.gender, 
 	s.salary
-from salaries s
-join employees e ON
+FROM salaries s
+JOIN employees e ON
 e.emp_no = s.emp_no;
+
+-- List employees who were hired in 1986.
+SELECT *
+FROM employees
+WHERE (
+	hire_date >= '1986/01/01' AND
+	hire_date <= '1986/12/31'
+);
+
